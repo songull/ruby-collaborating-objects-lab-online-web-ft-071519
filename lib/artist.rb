40827f542c48fd86f4352a35fa5b9b9f75c4945d
Kyler Barber <kylerbarber820@yahoo.com>
2019-08-13 23:24:49 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -1,37 +1,45 @@
 class Artist
-  attr_accessor :name
+  attr_accessor :name, :songs
 
-  @@all = []
+   @@all = []
 
-  def initialize(name)
+   def initialize(name)
     @name = name
-    @songs= []
+    @songs = []
+    save
   end
 
-  def add_song(song)
-    @songs << song
-    song.artist = self
-    song
-  end
-
-  def songs
-    @songs
-  end
-
-  def save
-    @@all << self
-    self
-  end
-
-  def self.all
+   def self.all
     @@all
   end
 
-  def self.find_or_create_by_name(name)
-    self.all.detect {|artist| artist.name == name} || Artist.new(name).save
+   def self.create(name)
+    artist = self.new(name)
+    artist.name = name
+    artist
   end
 
-  def print_songs
-    self.songs.each {|song| puts song.name}
-   end
-end
+   def self.find(name)
+    self.all.detect { |artist| artist.name == name }
+  end
+
+   def self.find_or_create_by_name(name)
+    self.find(name) || self.create(name)
+  end
+
+   def add_song(song)
+    @songs << song
+  end
+
+   def print_songs
+    songs.each { |song| puts song.name }
+  end
+
+   def save
+    @@all << self
+  end
+
+   def songs
+    @songs
+  end
+end 
